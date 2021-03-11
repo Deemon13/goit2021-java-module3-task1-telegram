8d0f1f166b0ce4351c04bcc4813fd4500d041f33
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-11 21:09:24 +0200

    added method testLogicalOperators() to class MoonComputerTests

--- src/main/java/MoonComputerTests.java
+++ src/main/java/MoonComputerTests.java
@@ -19,17 +19,38 @@ public class MoonComputerTests {
         System.out.println(sum + " " + sub + " " + mult + " " + div);
         scanner.close();
     }
+
+    public void testLogicalOperators() {
+        Scanner scanner = new Scanner(System.in);
+        boolean firstBool = scanner.nextBoolean();
+        boolean secondBool = scanner.nextBoolean();
+        boolean thirdBool = scanner.nextBoolean();
+
+        boolean andBool = firstBool && secondBool && thirdBool;
+        boolean orBool = firstBool || secondBool || thirdBool;
+
+        System.out.println(andBool + " " + orBool);
+
+        scanner.close();
+    }
+
     /*
     //Test output on Hello Java standard input
     public static void main(String[] args) {
         MoonComputerTests computerTests = new MoonComputerTests();
         computerTests.testBasicInputOutput();
     }
-
      */
 
+    /*
     //Test output on 24, 4 standard input
     public static void main(String[] args) {
         new MoonComputerTests().testMath();
+    }
+     */
+
+    //Test output on true, true, false standard input
+    public static void main(String[] args) {
+        new MoonComputerTests().testLogicalOperators();
     }
 }
