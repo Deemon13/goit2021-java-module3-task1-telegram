6f12c4725e1c03d657be4600cc79a352e05bdc97
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-11 19:47:38 +0200

    added method testMath() to class MoonComputerTests

--- src/main/java/MoonComputerTests.java
+++ src/main/java/MoonComputerTests.java
@@ -8,9 +8,26 @@ public class MoonComputerTests {
         scanner.close();
     }
 
+    public void testMath() {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int sum = firstNumber + secondNumber;
+        int sub = firstNumber - secondNumber;
+        int mult = firstNumber * secondNumber;
+        int div = firstNumber / secondNumber;
+        System.out.println(sum + " " + sub + " " + mult + " " + div);
+        scanner.close();
+    }
+
     //Test output on Hello Java standard input
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         MoonComputerTests computerTests = new MoonComputerTests();
         computerTests.testBasicInputOutput();
+    }
+
+    //Test output on 24, 4 standard input
+    public static void main(String[] args) {
+        new MoonComputerTests().testMath();
     }
 }
