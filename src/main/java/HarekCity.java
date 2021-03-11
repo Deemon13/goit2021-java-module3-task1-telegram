ab48ba0c67bcd6e93267ad5b34dc39705f067cf1
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-11 22:53:53 +0200

    added method firstAndLastTogether to class HarekCity

--- src/main/java/HarekCity.java
+++ src/main/java/HarekCity.java
@@ -16,6 +16,11 @@ public class HarekCity {
         names[3] = toReplace[1];
     }
 
+    public String firstAndLastTogether(String[] namesNew) {
+        String firstAndLast = namesNew[0] + " AND " + namesNew[9] + " TOGETHER";
+        return firstAndLast;
+    }
+
     public static void main(String[] args) {
         String[] arr = new HarekCity().createEmptyNameArray();
         System.out.println(Arrays.toString(arr));
@@ -27,5 +32,8 @@ public class HarekCity {
         String[] toReplace = new String[]{"lopr", "boki"};
         new HarekCity().fixNames(names, toReplace);
         System.out.println(Arrays.toString(names));
+
+        String[] namesNew = new String[]{"io", "pu", "re", "max", "kok", "mis", "hit", "kio", "pax", "rew"};
+        System.out.println(new HarekCity().firstAndLastTogether(namesNew));
     }
 }
