25f808dacaae98e0a77eae91744b11127199a941
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-13 22:58:29 +0200

    added class HarekDataMaker

---
+++ src/main/java/HarekDataMaker.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class HarekDataMaker {
+
+    public String aggregateSingle(String name, String age, String planet) {
+        return "name - " + name + ", age - " + age + ", planet - " + planet;
+    }
+
+    public String[] aggregateAll(String[] names, int[] ages, String[] planets) {
+        String firstEl = aggregateSingle(names[0], String.valueOf(ages[0]), planets[0]);
+        String secondEl = aggregateSingle(names[1], String.valueOf(ages[1]), planets[1]);
+        String thirdEl = aggregateSingle(names[2], String.valueOf(ages[2]), planets[2]);
+        String[] aggregateArray = new String[] {firstEl, secondEl, thirdEl};
+        return aggregateArray;
+    }
+
+    public static void main(String[] args) {
+       String[] names = new String[] {"hter", "pou", "diz"};
+        int[] ages = new int[] {30, 35, 70};
+        String[] planets = new String[] {"Mars", "Earth", "Jupiter"};
+
+        HarekDataMaker harekDataMaker = new HarekDataMaker();
+
+        System.out.println(harekDataMaker.aggregateSingle(names[0], Integer.toString(ages[0]), planets[0]));
+        System.out.println("###");
+
+        System.out.println(Arrays.toString(harekDataMaker.aggregateAll(names, ages, planets)));
+    }
+}
+
