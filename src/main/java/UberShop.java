49fd8974c6606881a5ef58310182c87978eea0a6
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-14 23:28:39 +0200

    added method findMinMaxPrices to class UberShop

--- src/main/java/UberShop.java
+++ src/main/java/UberShop.java
@@ -7,13 +7,39 @@ public class UberShop {
         }
     }
 
-    public void multiplyPrices(float[] prices) {
-        for (int i = 0; i <= prices.length - 1; i++) {
-            if (prices[i] < 1000) {
-                prices[i] = prices[i] * 2f;
+    public void multiplyPrices(float[] prices2) {
+        for (int i = 0; i <= prices2.length - 1; i++) {
+            if (prices2[i] < 1000) {
+                prices2[i] = prices2[i] * 2f;
             } else {
-                prices[i] = prices[i] * 1.5f;
+                prices2[i] = prices2[i] * 1.5f;
             }
+        }
+    }
+
+    public int[] findMinMaxPrices(int[] prices) {
+
+        if (prices.length == 0) {
+            return new int[0];
+        }
+
+        int maxEl = prices[0];
+        int minEl = prices[0];
+
+        for (int i = 0; i <= prices.length-1; i++) {
+            if (prices[i] > maxEl) {
+                maxEl = prices[i];
+            }
+
+            if (prices[i] < minEl) {
+                minEl = prices[i];
+            }
+        }
+
+         if (maxEl == minEl) {
+            return new int[] {maxEl};
+        } else {
+            return new int[] {minEl, maxEl};
         }
     }
 
@@ -27,9 +53,26 @@ public class UberShop {
         shop.printPrices(prices1);
 
         //Should be [200, 2250]
-        float[] prices = new float[] {100f, 1500f};
-        shop.multiplyPrices(prices);
-        System.out.println(Arrays.toString(prices));
+        float[] prices2 = new float[] {100f, 1500f};
+        shop.multiplyPrices(prices2);
+        System.out.println(Arrays.toString(prices2));
+
+        //Should be [50, 1500]
+        int[] prices = new int[] {100, 1500, 300, 50};
+        int[] minMax = shop.findMinMaxPrices(prices);
+        System.out.println(Arrays.toString(minMax));
+        //Should be [3, 1550]
+        int[] pricesTwo = new int[] {10, 50, 3, 1550};
+        int[] minMaxTwo = shop.findMinMaxPrices(pricesTwo);
+        System.out.println(Arrays.toString(minMaxTwo));
+        //Should be []
+        int[] pricesThree = new int[] {};
+        int[] minMaxThree = shop.findMinMaxPrices(pricesThree);
+        System.out.println(Arrays.toString(minMaxThree));
+        //Should be [50]
+        int[] pricesFour = new int[] {50, 50};
+        int[] minMaxFour = shop.findMinMaxPrices(pricesFour);
+        System.out.println(Arrays.toString(minMaxFour));
     }
 
 }
