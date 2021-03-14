107bf4444b1c825ed610762cd67056b3cf91b164
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-14 20:18:17 +0200

    added class UberShop

---
+++ src/main/java/UberShop.java
@@ -0,0 +1,16 @@
+public class UberShop {
+    public void printPrices(float[] prices) {
+        for(int i = 0; i <= prices.length - 1; i++) {
+            System.out.println(prices[i] + " jup.");
+        }
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        UberShop shop = new UberShop();
+
+        //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
+        float[] prices = new float[] {100f, 23.5f, 400f};
+        shop.printPrices(prices);
+    }
+}
