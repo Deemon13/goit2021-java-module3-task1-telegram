f544e71b9fa9f50c54614b66441ec040aa1c0239
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-14 21:03:55 +0200

    added method multiplyPrices to class UberShop

--- src/main/java/UberShop.java
+++ src/main/java/UberShop.java
@@ -1,16 +1,35 @@
+import java.util.*;
+
 public class UberShop {
-    public void printPrices(float[] prices) {
-        for(int i = 0; i <= prices.length - 1; i++) {
-            System.out.println(prices[i] + " jup.");
+    public void printPrices(float[] prices1) {
+        for(int i = 0; i <= prices1.length - 1; i++) {
+            System.out.println(prices1[i] + " jup.");
         }
     }
+
+    public void multiplyPrices(float[] prices) {
+        for (int i = 0; i <= prices.length - 1; i++) {
+            if (prices[i] < 1000) {
+                prices[i] = prices[i] * 2f;
+            } else {
+                prices[i] = prices[i] * 1.5f;
+            }
+        }
+    }
+
 
     //Test output
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
         //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
-        float[] prices = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(prices);
+        float[] prices1 = new float[] {100f, 23.5f, 400f};
+        shop.printPrices(prices1);
+
+        //Should be [200, 2250]
+        float[] prices = new float[] {100f, 1500f};
+        shop.multiplyPrices(prices);
+        System.out.println(Arrays.toString(prices));
     }
+
 }
