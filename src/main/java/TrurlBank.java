2de803bdae22f91beb0f7730ef6aa1c2859657d3
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-17 17:44:45 +0200

    added method countOfDigits to class TrurlBank

--- src/main/java/TrurlBank.java
+++ src/main/java/TrurlBank.java
@@ -9,6 +9,18 @@ public class TrurlBank {
         return sumOfQuads;
     }
 
+    public int countSumOfDigits(int number) {
+        int countSum = 0;
+        String numb = String.valueOf(number);
+        int i = 0;
+
+        while (i < numb.length()){
+            countSum += Integer.parseInt(String.valueOf(numb.charAt(i)));
+            i++;
+        }
+        return countSum;
+    }
+
     public static void main(String[] args) {
         TrurlBank bank = new TrurlBank();
 
@@ -16,5 +28,12 @@ public class TrurlBank {
         System.out.println(bank.sumQuads(3));
         //Should be 1 = 1*1
         System.out.println(bank.sumQuads(1));
+
+        //Should be 16 = 5 + 4 + 7
+        System.out.println(bank.countSumOfDigits(547));
+        //Should be 18 = 9 + 9
+        System.out.println(bank.countSumOfDigits(99));
+        //Should be 3 = 1 + 1 + 1
+        System.out.println(bank.countSumOfDigits(111));
     }
 }
